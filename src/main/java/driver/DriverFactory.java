682ca9d7a4ebc58f427456c03ab6ce720f4efd89
Nada Saad Mohy Saad Abd El Hamid <nada.saad@msa.edu.eg>
2026-05-20 16:13:15 +0300

    Update DriverFactory.java

--- src/main/java/driver/DriverFactory.java
+++ src/main/java/driver/DriverFactory.java
@@ -1,52 +1,118 @@
 package driver;
 
 import org.openqa.selenium.WebDriver;
+
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
+
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.Duration;
 
 public class DriverFactory {
 
-    public static WebDriver setDriver(String browser , String headless)
-    {
-        WebDriver driver ;
+    public static WebDriver initDriver(String browser, String headless, String executionType)  {
+
+        WebDriver driver;
+
+        boolean isHeadless = headless.equalsIgnoreCase("true");
+        boolean isRemote = executionType.equalsIgnoreCase("remote");
 
         switch (browser.toLowerCase()) {
-            case "chrome" -> {
+
+            case "chrome":
+
                 ChromeOptions chromeOptions = new ChromeOptions();
-                if (headless.equalsIgnoreCase("true")) {
-                    chromeOptions.addArguments("--headless");
+
+                if (isHeadless) {
+                    chromeOptions.addArguments("--headless=new");
                 }
-                driver = new ChromeDriver(chromeOptions);
-                System.out.println("Chrome Browser Started !!");
-            }
-            case "edge" -> {
+
+                if (isRemote) {
+                    try {
+                        driver = new RemoteWebDriver(
+                                new URL("http://localhost:4444"),
+                                chromeOptions
+                        );
+                    } catch (MalformedURLException e) {
+                        throw new RuntimeException(e);
+                    }
+                    System.out.println("Chrome browser launched successfully on Selenium Grid.");
+                } else {
+                    driver = new ChromeDriver(chromeOptions);
+                    System.out.println("Chrome browser launched successfully locally.");
+                }
+
+                break;
+
+            case "edge":
+
                 EdgeOptions edgeOptions = new EdgeOptions();
-                if (headless.equalsIgnoreCase("true")) {
-                    edgeOptions.addArguments("--headless");
+
+                if (isHeadless) {
+                    edgeOptions.addArguments("--headless=new");
                 }
-                driver = new EdgeDriver(edgeOptions);
-                System.out.println("Edge Browser Started !!");
-            }
-            case "firefox" -> {
+
+                if (isRemote) {
+                    try {
+                        driver = new RemoteWebDriver(
+                                new URL("http://localhost:4444"),
+                                edgeOptions
+                        );
+                    } catch (MalformedURLException e) {
+                        throw new RuntimeException(e);
+                    }
+                    System.out.println("Edge browser launched successfully on Selenium Grid.");
+                } else {
+                    driver = new EdgeDriver(edgeOptions);
+                    System.out.println("Edge browser launched successfully locally.");
+                }
+
+                break;
+
+            case "firefox":
+
                 FirefoxOptions firefoxOptions = new FirefoxOptions();
-                if (headless.equalsIgnoreCase("true")) {
+
+                if (isHeadless) {
                     firefoxOptions.addArguments("--headless");
                 }
-                driver = new FirefoxDriver(firefoxOptions);
-                System.out.println("FireFox Browser Started !!");
-            }
-            default -> throw new IllegalArgumentException("Unsupported browser: " + browser);
+
+                if (isRemote) {
+                    try {
+                        driver = new RemoteWebDriver(
+                                new URL("http://localhost:4444"),
+                                firefoxOptions
+                        );
+                    } catch (MalformedURLException e) {
+                        throw new RuntimeException(e);
+                    }
+                    System.out.println("Firefox browser launched successfully on Selenium Grid.");
+                } else {
+                    driver = new FirefoxDriver(firefoxOptions);
+                    System.out.println("Firefox browser launched successfully locally.");
+                }
+
+                break;
+
+            default:
+                throw new IllegalArgumentException("Unsupported browser: " + browser);
         }
+
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
         return driver;
     }
+
 
     public static WebDriver initDriver(String browser , String headless)
     {
